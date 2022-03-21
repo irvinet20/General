d127a34ddc1010babbcb0de1e51f6e7855a3e950
Cjspc2 <89712138+Cjspc2@users.noreply.github.com>
2022-03-21 14:41:14 -0500

    Add files via upload

---
+++ Personal/DiscordBots/FrogBot/WatcherFrog.py
@@ -0,0 +1,269 @@
+import os
+import os.path
+import time
+import random
+import discord
+from discord import *
+import random
+from discord.ext import commands
+
+TOKEN = 'x'
+
+cmdList = ['exclude ()', '.chatLog', '.test (arg)', '.magicBall ("arg?")',
+           '.roll (smallerNum LargerNum)', '.squaredCircle', '.Brunswick', '.helpList', 'x']
+           
+gmbList = ['exclude ()', '.gambleCoin (bet)']
+
+
+LawsonVote = 0
+DailyChatLog = []
+DailyChatLogLength = 0
+
+intents = discord.Intents.default()
+intents.members = True
+
+
+client = commands.Bot(intents=intents, command_prefix = '.')
+FrogBot = Client()
+
+
+#general = client.get_channel(846357507454140441)
+#serverIndex = client.guilds.index(846357507454140438)
+#homeIndex = server.channels.index(846357507454140441)
+
+#######
+#
+#Startup
+#
+#######
+@client.event
+async def on_ready():
+    print('test {0.user}'.format(client))
+    guild = client.get_guild(846357507454140438)
+    memberList = guild.members
+    memberNameList = []
+    for i in memberList:
+        memberNameList.append(i.name)
+    
+    if os.path.exists('GamblingAccounts.txt'):
+        await client.get_channel(882033829177597982).send(f'Accounts Found')
+        #with open('GamblingAccounts', 'r') as f:
+        #    for name in memberNameList:
+        #        if name not in f: #error with substring names
+        #            with open('GamblingAccounts', 'a') as e:
+        #                e.write(name + '=' + '1000')
+        #                e.write('\n')
+                    
+    else:
+        await client.get_channel(882033829177597982).send(f'No Accounts Found') 
+        with open('GamblingAccounts.txt', 'w') as f:
+            for members in memberList:
+                f.write(members.name + '=' + '1000')
+                f.write('\n')
+        f.close()
+    
+########
+#
+#Events
+#
+#######
+
+@client.event    
+async def on_message(message):
+    username = str(message.author).split('#')[0]
+    user_message = str(message.content)
+    channel = str(message.channel.name)
+    print(f'{username}: {user_message} ({channel})')
+
+    if message.author == client.user:
+        return
+        
+    DailyChatLog.append(f'{username}: {user_message} ({channel})')
+        
+    if user_message.lower() == 'hello':
+        await message.channel.send(f'Hello {username}!')
+        return
+
+    await client.process_commands(message)
+    return
+    
+#######
+#
+#General
+#
+#######
+
+@client.command()
+async def test(ctx, arg):
+    await ctx.send(arg)
+    
+@client.command()
+async def chatLog(ctx):
+    for i in DailyChatLog:
+        await ctx.send(i)
+        
+@client.command()
+async def magicBall(ctx, args):
+    check = random.randint(1,2)
+    if '?' not in args:
+        await ctx.send('correct formart is -> "question?"')
+        return
+    elif check == 1:
+        await ctx.send(args+' yes')
+    else:
+        await ctx.send(args+' no')
+        
+@client.command()
+async def roll(ctx, args1, args2):
+    if int(args1) > int(args2):
+        await ctx.send('please put the smaller number first')
+        return
+    retValue = random.randint(int(args1), int(args2))
+    await ctx.send(retValue)
+    
+#######
+#
+#Gambling
+#
+#######
+
+@client.command()
+async def gambleBalance(ctx):
+    asker = ctx.author.name
+    with open('GamblingAccounts.txt', 'r') as f:
+        for line in f:
+            (holder, bal) = line.split('=')
+            if holder == asker:
+                await ctx.send(bal)
+    f.close()
+            
+            
+@client.command()
+async def gambleCoin(ctx, args1): #check first then rewrite?
+    bettor = ctx.author.name
+    bet = int(args1)
+    rewrite = ''
+    with open('GamblingAccounts.txt', 'r') as f:
+        for line in f:
+            (holder, bal) = line.split('=')
+            if holder == bettor:
+                if bal >= args1:
+                    outcomeEvent = random.randint(0,1)
+                    if outcomeEvent == 1:
+                        newBal = str(int(bal)+bet)
+                        line = line.replace(bal, newBal)
+                        rewrite = rewrite+line+'\n'
+                        await ctx.send("winner, new balance "+newBal)
+                    else:
+                        newBal = str(int(bal)-bet)
+                        line = line.replace(bal, newBal)
+                        rewrite = rewrite+line+'\n'
+                        await ctx.send("loser, new balace "+newBal)
+                else:
+                    rewrite = rewrite+line
+                    await ctx.send("Not enough points")
+            else:
+                rewrite = rewrite+line
+    f.close()
+    with open('GamblingAccounts.txt', 'w') as f:
+        f.write(rewrite)
+        f.close()
+        
+@client.command()
+async def gambleGift(ctx, gift, reciever):
+    gifter = ctx.author.name
+    giftAmt = int(gift)
+    rewrite = ''
+    (validTransaction, validReciever) = validUserTransaction(ctx, int(gift), reciever)
+    if validTransaction and validReciever:
+        with open('GamblingAccounts.txt', 'r') as f:
+            for line in f:
+                (holder, bal) = line.split('=')
+                if holder == gifter:
+                    newBal = str(int(bal)-giftAmt)
+                    line = line.replace(bal, newBal)
+                    rewrite = rewrite+line+'\n'
+                elif holder == reciever:
+                    newBal = str(int(bal)+giftAmt)
+                    line = line.replace(bal, newBal)
+                    rewrite = rewrite+line+'\n'
+                else:
+                    rewrite = rewrite+line
+            f.close()
+        with open('GamblingAccounts.txt', 'w') as f:
+            f.write(rewrite)
+            f.close()
+        await ctx.send('Sent')
+    elif not validTransaction:
+        await ctx.send('not enough points'+str(validTransaction)+str(validReciever))
+    elif not validReciever:
+        await ctx.send('not a valid recipient')
+        
+####### 
+#
+#Point Spending
+#
+#######
+
+@client.command()
+async def pointTimeout(ctx, buyer, target):
+    return true
+    
+    
+#######
+#
+#Internals
+#
+#######
+
+#@param str, int
+def validUserTransaction(user, amt):
+    retValue = False
+    with open('GamblingAccounts.txt', 'r') as f:
+        for line in f:
+            (holder, bal) = line.split('=')
+            if holder == user:
+                if int(bal) >= amt:
+                    retValue = True
+        f.close()
+    return retValue
+
+#@param str, int, str
+def validUserTransaction(user, amt, tar):
+    retValue = [False, False]
+    with open('GamblingAccounts.txt', 'r') as f:
+        for line in f:
+            (holder, bal) = line.split('=')
+            if holder == user:
+                if int(bal) >= amt:
+                    retValue[0] = True
+            elif holder == tar:
+                retValue[1] == True
+        f.close()
+    return retValue
+
+
+
+#######
+#
+#Jokes
+#
+#######  
+
+@client.command()
+async def squaredCircle(ctx):
+    await ctx.send('A mythical object that is of a plane too high for Jake')
+    
+    
+    
+@client.command()
+async def helpList(ctx):
+    for i in cmdList:
+        await ctx.send(i)
+    
+    
+#@client.event
+#async def on_disconnect():
+#    await client.get_channel(846357507454140441).send(f'Dipping')
+#    
+client.run(TOKEN)
